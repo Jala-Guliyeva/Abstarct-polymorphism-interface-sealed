798f30bc0ac5867c9b342af66d3df33deccbc19c
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-03-17 23:17:49 +0400

    file added

---
+++ Abstract/Models/Figure.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract.Models
+{
+    abstract class Figure
+    {
+        public abstract int CalcArea(int area);
+    }
+}
---
+++ Abstract/Models/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstract.Models
+{
+    class Square:Figure
+    {
+        private int _side;
+        public Square(int side)
+        {
+            this.Side = side;
+        }
+
+        public Square()
+        {
+        }
+
+        public override int CalcArea(int area)
+        {
+            return area;
+        }
+        public int Side
+        {
+            get => _side;
+            
+            set
+            {
+                if (value > 0)
+                    _side = value;
+            }
+        }
+
+        internal int CalcArea(int area)
+        {
+            return area;
+        }
+
+        internal int CalcArea(string area)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
